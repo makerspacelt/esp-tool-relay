68028b41d96a1854e88345c0105a912ea4a92c5f
Justinas <laukejas@gmail.com>
2018-11-03 02:28:27 +0200

    Some evening changes, tipical...

--- Main.lua
+++ Main.lua
@@ -1,22 +1,23 @@
 require "RC522"
 
+--edit
 ID = nil
 station_cfg = {}
 station_cfg.ssid = "makerspace.lt"
 station_cfg.pwd = ""
-failedRetryCount = 0
+WifiConnectionRetryCount = 0
 timeEnabled = 0
 cardInserted = false
 cardGone = false
-removedCardTimer = 0
-removedCardTimerLimit = 10
+cardGoneTimer = 0
+cardGoneTimerLimit = 10
 relayEnabled = false
 permission = false
 waitingForAnswer = false
 ttl = 10
 connected = false
 reCheckConnectionTimer = 0
-reCheckConnectionTimerLimit = 300
+reCheckConnectionTimerLimit = 10
 macAddress = wifi.sta.getmac()
 header = ""
 -- End of config section
@@ -25,31 +26,23 @@ function createConnection() --TODO: check if resets when failed
     print("Creating connection...")
     wifi.sta.disconnect()
     wifi.setmode(wifi.NULLMODE)
+    tmr.delay(3000000)
     wifi.sta.config(station_cfg)
-    wifi.setmode(wifi.STATION)
+    wifi.setmode(wifi.STATION, false)
     wifi.sta.connect()
 end
 
 function checkConnection()
-    print("Connection status", wifi.sta.status())
-    if wifi.sta.status() == wifi.STA_GOTIP then --if connected
-        print("connected")
-        connected = true
-        failedRetryCount = 0
-        return
-    else                                        --if not connected
-        print("No connection... Retrying")
-        connected = false
-        tmr.delay(5000000)
-        failedRetryCount = failedRetryCount + 1
+    print("Checking connection...")
 
-        if failedRetryCount == 5 then
-            failedRetryCount = 0
-            createConnection()    
-        else
-            checkConnection()
-        end
-    end    
+    while wifi.sta.status() ~= wifi.STA_GOTIP do
+        print("Connection status", wifi.sta.status())
+        connected = false
+        tmr.delay(3000000)
+        createConnection() 
+        tmr.delay(3000000)
+    end 
+    print("Connection status", wifi.sta.status())
 end
 
 --------------------------------------------------------
@@ -62,72 +55,15 @@ function appendHex(t)
   return ""
 end
 
-function mainLoop()
-    --check connection
-    if connected == false then
-        tmr.interval(0, 5000)
-        checkConnection()
-        return
-    end
-
-    --re-check connection
-    reCheckConnectionTimer = reCheckConnectionTimer + 1
-    if reCheckConnectionTimer >= reCheckConnectionTimerLimit then
-        checkConnection()
-        reCheckConnectionTimer = 0
-        if connected == false then reCheckConnectionTimerLimit = 10 else reCheckConnectionTimerLimit = 30 end
-    end    
-    print("reCheckConnectionTimer ", reCheckConnectionTimer)
-    
-    
-    tmr.interval(0, 1000)
-
-    --get ID
-    oldID = ID
-    ID = RC522.get_ID() 
-    if ID == nil or ID == "" then
-        timeEnabled = 0
-        requestGot = false
-        permission = false
-        cardInserted = false 
-        if oldID ~= ID then 
-            print("oldID ", oldID, "ID ", ID)
-            cardGone = true 
-            print("card gone")
-        end
-    else
-        cardInserted = true
-        cardGone = false
-        removedCardTimer = 0
-        ID = appendHex(ID)
-    end
-
-    --beep if removedCardTimer running
-    if cardGone == true then
-        removedCardTimer = removedCardTimer + 1
-        if removedCardTimer >= removedCardTimerLimit then cardGone = false end
-        print("BEEP!")
-    end    
-    
-    --check for permission and enable relay
-    if permission == true and waitingForAnswer == false and cardInserted == true then
-        timeEnabled = timeEnabled + 1
-        print("Time enabled: ", timeEnabled)
-        relayEnabled = true
-    end
-
-    --disable relay
-    if permission == false and removedCardTimer >= removedCardTimerLimit then
-        relayEnabled = false
-    end
-
-    --send request and assign values
-    if timeEnabled == 0 or timeEnabled >= ttl then
-        if waitingForAnswer == false and cardInserted == true then
-            waitingForAnswer = true
+function sendRequestToServer()
+    if ID ~= nil and ID ~= "" then
+        if timeEnabled == 0 or timeEnabled >= ttl then
             timeEnabled = 0
             requestURL = "http://api.lan/api/v1/users/allowed/"..ID.."/"..macAddress
+            print("Sending request to server")
+            waitingForAnswer = true
             http.get(requestURL, "Authorization: Basic "..header.."\r\n", function(code, data)
+                print("Got answer from server")
                 if data ~= nil then
                     dataDecoded = cjson.decode(data)
                     for k,v in pairs(dataDecoded) do 
@@ -140,25 +76,96 @@ function mainLoop()
                 end
                 waitingForAnswer = false
             end)
-        end
-     end
+         end
+    end
+end
 
-    --enable relay
-    if relayEnabled == true then
+function checkPermissionFromServer()
+    --check for permission and enable relay
+    if permission == true and waitingForAnswer == false then
+        timeEnabled = timeEnabled + 1
+        print("Time enabled: ", timeEnabled)
+        relayEnabled = true
+    end
+
+    --disable relay
+    if permission == false then
+        relayEnabled = false
+    end
+end
+
+function getID()
+    oldID = ID
+    ID = RC522.get_ID() 
+    if ID == nil or ID == "" then
+        --timeEnabled = 0
+        --requestGot = false
+        --permission = false
+        --cardInserted = false 
+        if oldID ~= ID then 
+            cardGone = true 
+            print("Card gone!!!")
+        end
+    else
+        --cardInserted = true
+        --permission = true--for testing only
+        cardGone = false
+        cardGoneTimer = 0
+        ID = appendHex(ID)
+        print("ID: ", ID)
+    end
+
+    if cardGone == true then 
+        cardGoneTimer = cardGoneTimer + 1
+        print("BEEP!")
+    end
+    if cardGoneTimer >= cardGoneTimerLimit then 
+        cardGone = false 
+        permission = false
+    end  
+end
+
+function setRelay()
+    if permission == true then
         print("Relay Enabled")
         gpio.write(1, gpio.HIGH)
     else
         print("Relay Disabled")
         gpio.write(1, gpio.LOW)
     end
+end
+
+function mainLoop()
+    --check connection
+    reCheckConnectionTimer = reCheckConnectionTimer + 1 
+    if reCheckConnectionTimer >= reCheckConnectionTimerLimit then
+        reCheckConnectionTimer = 0
+        checkConnection() 
+    end
     
-    --TODO: move passwords to cfg
+    getID()
+
+    --send and check request from server
+
+    if waitingForAnswer == false then
+        sendRequestToServer()
+    else
+        checkPermissionFromServer()
+    end
+      
+    if permission == true then timeEnabled = timeEnabled + 1 end
+
+    
+    setRelay()
+
+
+    --TODO: move passwords to cfg, beeper
 end
     
 
 
 createConnection()
 
-tmr.alarm(0, 5000, tmr.ALARM_AUTO, function()
+tmr.alarm(0, 1000, tmr.ALARM_AUTO, function()
    mainLoop()
 end)
